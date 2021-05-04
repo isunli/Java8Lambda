029ea357cb683f029ac712bcadffae46e952edad
LS <ls@example.com>
2021-05-04 15:29:58 -0400

    finished

--- src/MyLambda/Main.java
+++ src/MyLambda/Main.java
@@ -1,7 +1,5 @@
 package MyLambda;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.*;
 
 public class Main {
@@ -46,35 +44,19 @@ public class Main {
 
         for (Employee employee : employees) {
             System.out.println(employee.getName());
-            // employee.setAge(4); // mutable class, but final
-            // employee = employees.get(1); // not final
-            new Thread(()-> System.out.println("From thread ----------"+employee.getAge())).start(); // here local varialbe employee is effictivly final
         }
 
-        employees.forEach(employee -> {
-            System.out.println(employee.getName());
-            System.out.println(employee.getAge());
-        });
 
-
-
-        String sillyString = doStringStuff((s1, s2) -> s1.toUpperCase() + s2.toUpperCase(), employees.get(0).getName(), employees.get(1).getName());
+        String sillyString = doStingStuff((s1, s2) -> s1.toUpperCase() + s2.toUpperCase(), employees.get(0).getName(), employees.get(1).getName());
         System.out.println(sillyString);
 
-        UpperConcat uc = (s1, s2) -> {
-            String result = s1.toUpperCase() + s2.toUpperCase(); // assumed return
-            return result;
-        };
-        sillyString = doStringStuff(uc, "aa", "bb");
+        UpperConcat uc = (s1, s2) -> s1.toUpperCase() + s2.toUpperCase(); // assumed return
+        sillyString = doStingStuff(uc, "aa", "bb");
         System.out.println(sillyString);
-
-        AnotherClass anotherClass = new AnotherClass();
-        String s = anotherClass.doSomething3();
-        System.out.println(s);
 
     }
 
-    public static String doStringStuff(UpperConcat uc, String s1, String s2) {
+    public static String doStingStuff(UpperConcat uc, String s1, String s2) {
         return uc.upperAndConcat(s1, s2);
     }
 
@@ -90,15 +72,8 @@ class Employee {
         this.age = age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public String getName() {
         return name;
-    }
-    public int getAge(){
-        return age;
     }
 }
 
@@ -111,75 +86,4 @@ class CodeToRun implements Runnable {
 
 interface UpperConcat {
     public String upperAndConcat(String s1, String s2);
-}
-
-class AnotherClass {
-    public String doSomething() {
-        System.out.println("The AnotherClass class's name is: " + getClass().getSimpleName());
-        return Main.doStringStuff(new UpperConcat() {
-            @Override
-            public String upperAndConcat(String s1, String s2) {
-                System.out.println("The anonymous class's name is: " + getClass().getSimpleName());
-                return s1.toUpperCase() + s2.toUpperCase();
-            }
-        }, "String1", "String2");
-    }
-
-    public String doSomething2() {
-        System.out.println("The AnotherClass class's name is: " + getClass().getSimpleName());
-        return Main.doStringStuff((s1, s2) -> {
-            System.out.println("The anonymous class's name is: " + getClass().getSimpleName());
-            return s1.toUpperCase() + s2.toUpperCase();
-
-        }, "String1", "String2"); // lambda is treated as a nested block of code instead of a new class
-    }
-
-    public String doSomething3(){
-        final int i=0;
-        int i2 = 0;
-
-        UpperConcat uc2 = (s1,s2)->{
-
-            System.out.println(i2);
-            return s1.toUpperCase()+s2.toUpperCase();
-        }; // imagine this code is running inside a code block
-
-        {
-            UpperConcat uc = new UpperConcat() {
-                @Override
-                public String upperAndConcat(String s1, String s2) {
-                    System.out.println("i (within anonymous class) = "+i);
-                    // local variable in anonymous class must be
-                    // final (value copied from local variable to anonymous class,
-                    // will get out of sync)
-
-                    return s1.toUpperCase()+s2.toUpperCase();
-                }
-
-
-            };
-
-
-
-
-
-            return Main.doStringStuff(uc, "String1","String2");
-        }
-
-
-
-    }
-    public void printValue() {
-        int number = 25;
-        Runnable r = () -> {
-            try{
-                Thread.sleep(5000);
-            } catch (InterruptedException e){
-                e.printStackTrace();
-            }
-            System.out.println("The value is "+number);
-        };
-        new Thread(r).start();
-    }
-
 }
---
+++ src/MyStreams/Main.java
@@ -0,0 +1,104 @@
+package MyStreams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> someBingoNumbers = Arrays.asList("N40", "N36",
+                "B12", "B6",
+                "G53", "G49", "G60", "G50", "g64",
+                "I26", "I17", "I29",
+                "O71");
+        List<String> gNumbers = new ArrayList<>();
+        someBingoNumbers.forEach(e->{
+            if(e.toUpperCase().startsWith("G")){
+                gNumbers.add(e);
+                //System.out.println(e);
+            }
+        });
+
+        gNumbers.sort((s1,s2)->s1.compareTo(s2));
+        gNumbers.forEach(s-> System.out.println(s));
+
+        // Stream
+        someBingoNumbers
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s->s.startsWith("G"))
+                .sorted()
+                .forEach(System.out::println); // terminal operation
+
+        Stream<String> ioNumberStream = Stream.of("I26","I17","I29","O71");
+        Stream<String> inNumberStream = Stream.of("N40","N36","I26","I17","I29","O71");
+        Stream<String> concatStream = Stream.concat(ioNumberStream,inNumberStream);
+        //System.out.println(concatStream.distinct().count());
+        // peak is not a terminal operation
+        System.out.println("----------------------------------");
+        System.out.println(concatStream.distinct().peek(System.out::println).count());
+
+        Employee john = new Employee("John Dow", 30);
+        Employee jane = new Employee("Jane Deer", 30);
+        Employee jack = new Employee("Jack Hill", 30);
+        Employee snow = new Employee("Snow White", 30);
+
+        Department hr = new Department("Human Resources");
+        hr.addEmployee(john);
+        hr.addEmployee(jack);
+        hr.addEmployee(snow);
+        Department accounting = new Department("Accounting");
+        accounting.addEmployee(john);
+
+        List<Department> departments = new ArrayList<>();
+        departments.add(hr);
+        departments.add(accounting);
+
+        departments.stream()
+                .flatMap(d -> d.getEmployees().stream())
+                .forEach(System.out::println);
+
+//        List<String> sortedGNumbers = someBingoNumbers
+//                .stream()
+//                .map(String::toUpperCase)
+//                .filter(s -> s.startsWith("G"))
+//                .sorted()
+//                .collect(Collectors.toList());
+        List<String> sortedGNumbers = someBingoNumbers
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                // constructor, add, combine
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        for (String s: sortedGNumbers){
+            System.out.println(s);
+        }
+        Map<Integer, List<Employee>> groupedByAge = departments
+                .stream()
+                .flatMap(d -> d.getEmployees().stream())
+                .collect(Collectors.groupingBy(employee -> employee.getAge()));
+
+
+        departments.stream() // get yougest employee
+                .flatMap(d -> d.getEmployees().stream())
+                .reduce((e1,e2)->e1.getAge() < e2.getAge() ? e1 : e2)
+                .ifPresent(System.out::println); // all operation is lazy, will perform on terminal operation
+
+        var s1 = Stream.of("ABC","AC","BAA","CCCC","XY","ST")
+                .filter(s->{
+                    System.out.println(s);
+                    return s.length() == 3;
+                });
+        System.out.println("starting terminal op");
+        s1.count();
+
+
+
+
+
+    }
+}
